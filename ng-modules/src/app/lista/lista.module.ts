990766a9ef82a8e6451a6d9590c4d4d39f779a59
joaomarcos2004.jonh@gmail.com <joaomarcos2004.jonh@gmail.com>
2024-02-14 01:15:02 -0300

    testing modules

--- ng-modules/src/app/lista/lista.module.ts
+++ ng-modules/src/app/lista/lista.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './input/input.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InputComponent,
+    ListagemComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    InputComponent,
+    ListagemComponent
   ]
 })
 export class ListaModule { }
